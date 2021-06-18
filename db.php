c78f59fe9dafef62fe03fc06e7c9eb0b7fa724dd
shkhan922 <shkhan922@gmail.com>
2021-06-18 15:02:33 +0530

    db config added

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
-$conn=mysqli_connect("localhost","winderm1_software","Welcome@2121@") or die("Could not connect");
+$conn=mysqli_connect("tminspector-database-1.cjalh6h9wz7x.us-east-2.rds.amazonaws.com","admin","gXDuqPpAgqKoe1iHsM6I") or die("Could not connect");
 mysqli_select_db($conn,"winderm1_software") or die("could not connect database");
 ?>
